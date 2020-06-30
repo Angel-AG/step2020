1c4c0ac5934788b7952cd9d0ba11aad3d2a5fca2
Angel Guevara <angelguevarahdz.io@gmail.com>
2020-06-30 07:13:00 +0000

    Divide functions - Add timeout and error handler

--- portfolio/src/main/webapp/reusehtml.js
+++ portfolio/src/main/webapp/reusehtml.js
@@ -13,6 +13,62 @@
 // limitations under the License.
 
 /**
+ * Alters the inner html of an HTML element, depending on the 
+ * request's state and the HTTP response status code.
+ * XML HTTP Request states:
+ * - 0 UNSET
+ * - 1 OPENED
+ * - 2 HEADERS_RECEIVED
+ * - 3 LOADING
+ * - 4 DONE
+ * @param {Object} xmlHttp XMLHttpRequest to monitor the progress
+ * @param {Object} element HTML element to alter its inner content
+ */
+function alterHtml(xmlHttp, element) {
+  if (xmlHttp.readyState < 4) {
+    element.innerHTML = "Loading...";
+  } else if (xmlHttp.readyState === 4) {
+    if (xmlHttp.status === 200) {
+      element.innerHTML = xmlHttp.responseText;
+    } else if (xmlHttp.status === 404) {
+      element.innerHTML = "Page not found";
+    } else if (xmlHttp.status === 0) {
+      element.innerHTML = "Timeout. Unable to reach server";
+    } else {
+      element.innerHTML = "Something unexpected ocurred " + 
+      xmlHttp.status + " " + xmlHttp.statusText;
+    }
+    
+    element.removeAttribute("include-html");
+  }
+}
+
+/**
+ * Make an HTTP Request for the HTML file.
+ * @param {Object} htmlElement HTML element to alter its content
+ * @param {string} htmlPath Path to the HTML file
+ * @param {function} timeoutFunc Function to fire when timeout is reached
+ */
+function requestHtmlFile(htmlElement, htmlPath, timeoutFunc) {
+  const xmlHttp = new XMLHttpRequest();
+  
+  xmlHttp.onreadystatechange = () => {
+    alterHtml(xmlHttp, htmlElement);
+  };
+
+  xmlHttp.open("GET", htmlPath, true);
+
+  xmlHttp.onerror = () => {
+    alert("Sorry. An error ocurred during the request");
+  };
+  
+  xmlHttp.timeout = 5000; // milliseconds 
+  xmlHttp.ontimeout = timeoutFunc;
+
+  xmlHttp.send();
+}
+
+/**
  * Adds HTML content to elements with the include-html attribute
  */
 function includeHtml() {
@@ -24,18 +80,9 @@ function includeHtml() {
     
     // Check path is not an empty string
     if (path) {
-      // Make an HTTP Request for the HTML file
-      const xmlHttp = new XMLHttpRequest();
-      xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4) {
-          if (xmlHttp.status === 200) {element.innerHTML = xmlHttp.responseText;};
-          if (xmlHttp.status === 404) {element.innerHTML = "Page not found";};
-        
-          element.removeAttribute("include-html");
-        }
-      };
-      xmlHttp.open("GET", path, true);
-      xmlHttp.send();
+      // Two tries to request the file
+      const repeat = function() {requestHtmlFile(element, path, () => {});};
+      requestHtmlFile(element, path, repeat);
     }
   }
 }
