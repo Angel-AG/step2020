c82ad387676c2fc0ac40b3dfd9d6f57245205f90
Angel Guevara <angelguevarahdz.io@gmail.com>
2020-07-07 04:09:19 +0000

    Address PR comments - AddCommentsServlet

--- portfolio/src/main/java/com/google/sps/servlets/AddCommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AddCommentsServlet.java
@@ -17,6 +17,7 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.sps.data.HttpServletUtils;
 import java.io.IOException;
 import java.util.Date;
 import javax.servlet.annotation.WebServlet;
@@ -28,25 +29,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/add-comments")
 public class AddCommentsServlet extends HttpServlet {
 
-  // POST parameters
-  private String username;
-  private String comment;
-  private String imageId;
-
-  private Date date;
-
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    handlePostParams(request);
-    date = new Date();
-
+    // Handle POST parameters
+    String username = HttpServletUtils.getParameter(request, "username", "Anonymous");
+    String comment = HttpServletUtils.getParameter(request, "comment", "");
+    String imageId = HttpServletUtils.getParameter(request, "imageId", "");
+    
     // Send an error message
     if (imageId.isEmpty() || comment.isEmpty()) {
       response.sendError(HttpServletResponse.SC_BAD_REQUEST);
       return;
     }
 
-    Entity commentEntity = createCommentEntity();
+    Entity commentEntity = createCommentEntity(username, comment, imageId);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
@@ -55,35 +51,14 @@ public class AddCommentsServlet extends HttpServlet {
   }
 
   /**
-   * Get and assign parameters of a request
-   */
-  private void handlePostParams(HttpServletRequest request) {
-    username = getParameter(request, "username", "Anonymous");
-    comment = getParameter(request, "comment", "");
-    imageId = getParameter(request, "imageId", "");
-  }
-
-  /**
-   * Return the request parameter or the default value if the parameter
-   * was not specified by the client
-   */
-  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
-    String value = request.getParameter(name);
-    if (value == null || value.isEmpty()) {
-      return defaultValue;
-    }
-    return value;
-  }
-
-  /**
    * Return a comment entity
    */
-  private Entity createCommentEntity() {
+  private Entity createCommentEntity(String username, String comment, String imageId) {
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("username", username);
     commentEntity.setProperty("comment", comment);
     commentEntity.setProperty("imageId", imageId);
-    commentEntity.setProperty("date", date);
+    commentEntity.setProperty("date", new Date());
     
     return commentEntity;
   }
